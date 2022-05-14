e55c41118af9b478f9b8db14330fa17373ddd6ab
Oscar S <ozcarssss@gmail.com>
2022-05-14 16:30:23 +0200

    some style tweaks

--- main.js
+++ main.js
@@ -5,6 +5,8 @@ var canvasHeight = canvas.height;
 
 var canvasData = ctx.getImageData(0, 0, canvasWidth, canvasHeight);
 
+var lastType = "";
+
 function drawPixel (x, y, r, g, b, a) {
     var index = (x + y*canvasWidth)*4;
     canvasData.data[index + 0] = r;
@@ -15,9 +17,7 @@ function drawPixel (x, y, r, g, b, a) {
 
 function clearPixels () {
     for (let i = 0; i < canvasData.data.length; i+=4){
-        canvasData.data[i] = 255;
-        canvasData.data[i + 1] = 255;
-        canvasData.data[i + 2] = 255;
+        canvasData.data[i] = canvasData.data[i + 1] = canvasData.data[i + 2] = 255;
     }
     updateCanvas();
 }
@@ -31,80 +31,52 @@ function initTriangle() {
     drawPixel(0, canvasHeight-1, 255, 1, 1, 255);
     drawPixel(canvasWidth-1, canvasHeight-1, 255, 1, 1, 255);
     updateCanvas();
-}
+}   
 
 function drawSierpinski(type, points) {
     initTriangle();
-    var triPoint1 = [canvasHeight*0.5, 1];
-    var triPoint2 = [1, canvasHeight-2];
-    var triPoint3 = [canvasWidth-1, canvasHeight-1];
 
-    var tmpPoint = triPoint1;
-    var tmpPointTri = triPoint2;
+    var triPoint = [[canvasHeight*0.5, 1], [1, canvasHeight-2], [canvasWidth-1, canvasHeight-1]];
 
-    function newPointGen(){
-       
-        var colorR = 1;
-        var colorG = 1;
-        var colorB = 1;            
+    var tmpPoint = triPoint[0];
+    var tmpPointTri = triPoint[1];
 
-        var newPoint = [Math.ceil((tmpPointTri[0]>tmpPoint[0] ? (tmpPointTri[0]+tmpPoint[0])/2 : (tmpPoint[0]+tmpPointTri[0])/2)), 
+    function newPointGen(){            
+
+        let newPoint = [Math.ceil((tmpPointTri[0]>tmpPoint[0] ? (tmpPointTri[0]+tmpPoint[0])/2 : (tmpPoint[0]+tmpPointTri[0])/2)), 
                         Math.ceil(tmpPointTri[1]>tmpPoint[1] ? (tmpPointTri[1]+tmpPoint[1])/2 : (tmpPoint[1]+tmpPointTri[1])/2)];
-        if (newPoint[0]>canvasWidth*0.5){
-            colorR = ((newPoint[0]-canvasWidth*0.5)/canvasWidth)*255;
-            colorG = ((newPoint[0]-canvasWidth*0.5)/canvasWidth)*255+100;
-            colorB = ((newPoint[0]-canvasWidth*0.5)/canvasWidth)*255+50;
-        }
-        else {
-            colorR = ((canvasWidth*0.5-newPoint[0])/canvasWidth)*255;
-            colorG = ((canvasWidth*0.5-newPoint[0])/canvasWidth)*255+100;
-            colorB = ((canvasWidth*0.5-newPoint[0])/canvasWidth)*255+50;
-        }
+        
+        let colorR = (newPoint[0]>canvasWidth*0.5) ? ((newPoint[0]-canvasWidth*0.5)/canvasWidth)*255 + 100 : ((canvasWidth*0.5-newPoint[0])/canvasWidth)*255 + 100;
+        let colorG = (newPoint[0]>canvasWidth*0.5) ? ((newPoint[0]-canvasWidth*0.5)/canvasWidth)*1 : ((canvasWidth*0.5-newPoint[0])/canvasWidth)*1;
+        let colorB = (newPoint[0]>canvasWidth*0.5) ? ((newPoint[0]-canvasWidth*0.5)/canvasWidth)*1 : ((canvasWidth*0.5-newPoint[0])/canvasWidth)*1;
+
         drawPixel(newPoint[0], newPoint[1], colorR, colorG, colorB, 255);
         
         tmpPoint = newPoint;
-
-        tmp = Math.floor(Math.random()*3)+1;
-        switch(tmp) {
-        case 1:
-            tmpPointTri = triPoint1;
-            break;
-        case 2:
-            tmpPointTri = triPoint2;
-            break;
-        case 3:
-            tmpPointTri = triPoint3;
-            break;
-        default:
-            tmpPointTri = triPoint1;
-        }
+        tmpPointTri = triPoint[Math.floor(Math.random()*3)];
     }
 
-    var drawTriangle;
     if (type == "scatter"){
-        ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-        clearTimeout(drawTriangle);
-        drawTriangle = setInterval(() => { drawInterval() }, 1);
-        setTimeout(() => {
-            clearInterval(drawTriangle);
-            return;
-        }, 1000);
+        if (lastType != "scatter"){
+            clearPixels();
+        }
+        lastType = "scatter";
+        var drawTriangle = setInterval(() => { drawInterval() }, 1);
+        setTimeout(() => { clearInterval(drawTriangle) }, 1000);
     
         function drawInterval() {
-            for (let i = 0; i<100; i++){
-                newPointGen();
-            }
+            for (let i = 0; i<100; i++) newPointGen();
             updateCanvas();
         }
     }
     else if(type == "instant") {
-        for (let i = 0; i<points; i++){
-            newPointGen();
-        }
+        clearPixels();
+        for (let i = 0; i<points; i++) newPointGen();
         updateCanvas();
-        return;
+        lastType = "instant"
     }
     else if (type == "clear"){
+        lastType = "clear";
         clearPixels();    
     }
 }
